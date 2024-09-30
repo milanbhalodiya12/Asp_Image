e4e1a5d232e6d90296f0fcc5dfad465d4eafa9d6
unknown <milan.mca111@gmail.com>
2024-09-30 09:15:59 +0530

    Image_uplode

---
+++ imgAdvancedaspnet/imgAdvancedaspnet/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using imgAdvancedaspnet.Models;
+
+namespace imgAdvancedaspnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Student s = new Student();
+            return View(s);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Student s , HttpPostedFileBase img)
+        {
+            var db = new mydbEntities();
+            if(img != null)
+            {
+                s.image = new byte[img.ContentLength];
+                img.InputStream.Read(s.image, 0, img.ContentLength);
+            }
+            db.Students.Add(s);
+            db.SaveChanges();
+            return View(s);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
